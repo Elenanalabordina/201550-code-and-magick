4d374cc08d2a3635e844ae847b9d48abdd21e668
“Elena <elenanalabordina@mail.ru>
2017-11-21 11:38:30 +0700

    ошибки eslint

--- js/stat.js
+++ js/stat.js
@@ -4,29 +4,25 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)'; // тень;
   ctx.strokeRect(110, 20, 420, 270);
   ctx.fillRect(110, 20, 420, 270);
-  
   ctx.fillStyle = 'rgba(256, 256, 256, 1.0)'; // white;
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
-  
   ctx.fillStyle = '#000'; // black;
   ctx.font = '16px PT Mono';
-
   ctx.fillText('Ура вы победили!', 150, 30);
   ctx.fillText('Cписок результатов: ', 150, 60);
   
   var max = -1;
   var maxIndex = -1;
 
-  for (var i = 0 ; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
       maxIndex = i;
     }
   }
-  
-  var histogramWidth = 150;              //высота гистограммы px;
+  var histogramWidth = 150; //высота гистограммы px;
   var step = histogramWidth / (max - 0); //пропорция  px;
   var barWidth = 40; //ширина столбца px; 
   var indent = barWidth + 50; //расстояние между столбцами px;
