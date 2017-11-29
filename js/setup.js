a6a2a2b398d11ec6ecf9a4a0b4396710abe3a78e
“Elena <elenanalabordina@mail.ru>
2017-11-29 11:16:51 +0700

    пробелы

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var WIZARD_NAMES = [['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']];
+var WIZARD_NAMES = [['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф',  'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']];
 
 var WIZARD_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
@@ -19,8 +19,7 @@ var calculationRandomElement = function () {
 var calculationRandomElements = function () {
   var surnames = WIZARD_NAMES[1];
   return Math.floor(Math.random() * surnames.length);
-};
-  
+}; 
   
 var calculationRandomElement1 = function () {
   return Math.floor(Math.random() * WIZARD_COLORS.length);
@@ -33,18 +32,18 @@ var wizards = [{
   name: WIZARD_NAMES[0][calculationRandomElement()] + ' ' + WIZARD_NAMES[1][calculationRandomElement()],
   coatColor: WIZARD_COLORS[calculationRandomElement1()],
   eyesColor: WIZARD_EYES[calculationRandomElement2()]
-  },
-  {
+},
+{
   name: WIZARD_NAMES[0][calculationRandomElement ()] + ' ' + WIZARD_NAMES[1][calculationRandomElement()],
   coatColor: WIZARD_COLORS[calculationRandomElement1()],
   eyesColor: WIZARD_EYES[calculationRandomElement2()] 
-  },
-  {
+},
+{
   name: WIZARD_NAMES[0][calculationRandomElement()] + ' ' + WIZARD_NAMES[1][calculationRandomElement()],
   coatColor: WIZARD_COLORS[calculationRandomElement1()],
   eyesColor: WIZARD_EYES[calculationRandomElement2()]
-  },
-  {
+},
+{
   name: WIZARD_NAMES[0][calculationRandomElement()] + ' ' + WIZARD_NAMES[1][calculationRandomElement()],
   coatColor: WIZARD_COLORS[calculationRandomElement1()],
   eyesColor: WIZARD_EYES[calculationRandomElement2()] 
